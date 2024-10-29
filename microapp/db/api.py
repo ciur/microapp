1344b360c41cb7e5a7b657ac0fda73238fd36c78
Eugen Ciur <eugen@papermerge.com>
2024-10-29 07:13:42 +0100

    got it !

--- microapp/db/api.py
+++ microapp/db/api.py
@@ -1,9 +1,10 @@
+import uuid
 from microapp.db.orm import User
 from microapp import models
 
 
 def create_user(session, username: str, email: str) -> models.User:
-    user = User(username=username, email=email)
+    user = User(id=uuid.uuid4(), username=username, email=email)
     session.add(user)
     session.commit()
 
